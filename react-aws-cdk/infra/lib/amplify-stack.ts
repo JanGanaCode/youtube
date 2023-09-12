a6f83d5fdcbee2e9b88d6da90874c5c67da70af4
Jan Gana <jangana.code@gmail.com>
2023-09-12 03:56:33 +0100

    init

--- react-aws-cdk/infra/lib/amplify-stack.ts
+++ react-aws-cdk/infra/lib/amplify-stack.ts
@@ -3,19 +3,19 @@ import { Construct } from "constructs";
 import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 import { App, GitHubSourceCodeProvider } from "@aws-cdk/aws-amplify-alpha";
 
-export class AdminAppAmplifyStack extends Stack {
+export class AmplifyStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const amplifyAppName = "Admin App";
+    const amplifyAppName = "test App";
 
     const amplifyApp = new App(this, amplifyAppName, {
       environmentVariables: {
-        AMPLIFY_MONOREPO_APP_ROOT: "frontend",
+        AMPLIFY_MONOREPO_APP_ROOT: "react-aws-cdk/frontend",
       },
       sourceCodeProvider: new GitHubSourceCodeProvider({
         owner: "janganacode",
-        repository: "youtube/test",
+        repository: "youtube",
         oauthToken: SecretValue.secretsManager("github-token"),
       }),
       buildSpec: BuildSpec.fromObjectToYaml({
@@ -39,10 +39,11 @@ export class AdminAppAmplifyStack extends Stack {
                 paths: ["node_modules/**/*"],
               },
             },
-            appRoot: "frontend",
+            appRoot: "react-aws-cdk/frontend",
           },
         ],
       }),
     });
+    const mainBranch = amplifyApp.addBranch("react-aws-cdk");
   }
 }
